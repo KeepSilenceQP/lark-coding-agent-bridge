bihangchi9-creator/trae-to-lark

feat: support bot meeting

src/ui/api.ts
@@ -11,6 +11,10 @@ import {
   startDeviceLogin,
   type AddBotResult,
 } from '../lark-cli/user-im';
+import { isMeetingNo } from '../meeting/api';
+import { checkMeetingPreflight, type MeetingPreflight } from '../meeting/preflight';
+import { describeMeetingError, type MeetingManager, type MeetingPushHealth } from '../meeting/manager';
+import type { MeetingSessionStatus } from '../meeting/session';
 import { resolveAppPaths } from '../config/app-paths';
 import { loadRootConfig, runtimeProfileConfig } from '../config/profile-store';
 import {
@@ -32,6 +36,7 @@ import {
 } from '../config/schema';
 import {
   effectiveLarkCliIdentity,
+  type MeetingConfig,
   type LarkCliIdentityPreset,
   type ProfileAccess,
   type ProfileMode,
@@ -58,6 +63,7 @@ export interface ConfigView {
   runIdleTimeoutMinutes: number;
   requireMentionInGroup: boolean;
   larkCliIdentity: LarkCliIdentityPreset;
+  meeting: MeetingConfig;
   access: {
     allowedUsers: string[];
     allowedChats: string[];
@@ -86,6 +92,7 @@ export function buildConfigView(state: MutableProfileState, live = false): Confi
     runIdleTimeoutMinutes: ms ? Math.round(ms / 60_000) : 0,
     requireMentionInGroup: getRequireMentionInGroup(state.cfg),
     larkCliIdentity: state.profileConfig.larkCli.identityPreset,
+    meeting: state.profileConfig.meeting,
     access: {
       allowedUsers: state.profileConfig.access.allowedUsers,
       allowedChats: state.profileConfig.access.allowedChats,
@@ -141,8 +148,46 @@ function clampInt(value: unknown, min: number, max: number, fallback: number): n
   return Math.min(max, Math.max(min, Math.floor(n)));
 }
 
+/**
+ * Parse the in-meeting agent section. Unspecified fields keep their current
+ * value; numbers are clamped to the same bounds the schema normalizer uses.
+ */
+function parseMeetingBody(body: unknown, current: MeetingConfig): MeetingConfig {
+  if (!body || typeof body !== 'object' || Array.isArray(body)) return current;
+  const fv = body as Record<string, unknown>;
+  const t = (fv.transcript && typeof fv.transcript === 'object' ? fv.transcript : {}) as Record<string, unknown>;
+  const trigger = typeof fv.trigger === 'string' && fv.trigger.trim() ? fv.trigger.trim() : current.trigger;
+  return {
+    enabled: typeof fv.enabled === 'boolean' ? fv.enabled : current.enabled,
+    autoJoinOnInvite:
+      typeof fv.autoJoinOnInvite === 'boolean' ? fv.autoJoinOnInvite : current.autoJoinOnInvite,
+    transcript: {
+      keep: t.keep === undefined ? current.transcript.keep : clampInt(t.keep, 10, 2000, current.transcript.keep),
+      stabilizeMs:
+        t.stabilizeMs === undefined
+          ? current.transcript.stabilizeMs
+          : clampInt(t.stabilizeMs, 0, 30_000, current.transcript.stabilizeMs),
+    },
+    respondIn:
+      fv.respondIn === 'meeting' || fv.respondIn === 'im' || fv.respondIn === 'both'
+        ? fv.respondIn
+        : current.respondIn,
+    trigger,
+    pollIntervalMs:
+      fv.pollIntervalMs === undefined
+        ? current.pollIntervalMs
+        : clampInt(fv.pollIntervalMs, 1000, 60_000, current.pollIntervalMs),
+    summaryOnEnd: typeof fv.summaryOnEnd === 'boolean' ? fv.summaryOnEnd : current.summaryOnEnd,
+    summaryTarget:
+      fv.summaryTarget === 'origin' || fv.summaryTarget === 'owner'
+        ? fv.summaryTarget
+        : current.summaryTarget,
+  };
+}
+
 interface ParsedConfig {
   mode: ProfileMode;
+  meeting: MeetingConfig;
   larkCliIdentity: LarkCliIdentityPreset;
   requireMentionInGroup: boolean;
   nextPreferences: AppPreferences;
@@ -205,11 +250,14 @@ function parseConfigBody(state: MutableProfileState, body: unknown): ParsedConfi
       ? fv.requireMentionInGroup
       : getRequireMentionInGroup(state.cfg);
 
+  const meeting = parseMeetingBody(fv.meeting, state.profileConfig.meeting);
+
   const nextEffectiveIdentity: LarkCliIdentityPreset = mode === 'team' ? 'bot-only' : larkCliIdentity;
   const previousEffectiveIdentity = effectiveLarkCliIdentity(state.profileConfig);
 
   return {
     mode,
+    meeting,
     larkCliIdentity,
     requireMentionInGroup,
     nextEffectiveIdentity,
@@ -242,7 +290,14 @@ export async function applyConfig(rt: UiRuntime, body: unknown): Promise<ConfigV
       if (!ok) throw new ApiError(500, 'lark-cli 身份策略未生效');
       identityApplied = true;
     }
-    await savePreferencesConfig(rt, p.nextPreferences, p.requireMentionInGroup, p.larkCliIdentity, p.mode);
+    await savePreferencesConfig(
+      rt,
+      p.nextPreferences,
+      p.requireMentionInGroup,
+      p.larkCliIdentity,
+      p.mode,
+      p.meeting,
+    );
   } catch (err) {
     if (identityApplied) {
       await applyProfileLarkCliIdentity(rt, p.previousEffectiveIdentity).catch(() =>
@@ -269,7 +324,14 @@ export async function applyConfigToDisk(
 ): Promise<ConfigView> {
   const p = parseConfigBody(state, body);
   try {
-    await savePreferencesConfig(state, p.nextPreferences, p.requireMentionInGroup, p.larkCliIdentity, p.mode);
+    await savePreferencesConfig(
+      state,
+      p.nextPreferences,
+      p.requireMentionInGroup,
+      p.larkCliIdentity,
+      p.mode,
+      p.meeting,
+    );
   } catch (err) {
     throw new ApiError(500, `保存失败：${err instanceof Error ? err.message : String(err)}`);
   }
@@ -358,6 +420,86 @@ export async function listChats(channel: LarkChannel | undefined) {
   if (!channel) return { chats: [] };
   const chats = await fetchKnownChats(channel).catch(() => []);
   return { chats };
+}
+
+// ── in-meeting agent ─────────────────────────────────────────────────────────
+
+export interface MeetingsView {
+  /** False when the profile is offline or `meeting.enabled` is off. */
+  available: boolean;
+  reason?: string;
+  sessions: MeetingSessionStatus[];
+  push: MeetingPushHealth;
+}
+
+/**
+ * Live meeting state for a profile. Needs the running process's manager, so an
+ * offline profile reports `available: false` rather than erroring.
+ */
+export function meetingsView(manager: MeetingManager | undefined, enabled: boolean): MeetingsView {
+  if (!enabled) {
+    return {
+      available: false,
+      reason: '该 profile 未启用会议智能体',
+      sessions: [],
+      push: { hooked: false, received: 0 },
+    };
+  }
+  if (!manager) {
+    return {
+      available: false,
+      reason: 'profile 未在线（会议能力只在运行中的进程里可用）',
+      sessions: [],
+      push: { hooked: false, received: 0 },
+    };
+  }
+  return { available: true, sessions: manager.list(), push: manager.pushHealth() };
+}
+
+/**
+ * Whether the app identity can actually join meetings. Runs a read-only probe;
+ * when a scope is missing the result carries Feishu's own scope-apply URL so
+ * the console can offer a one-click grant.
+ */
+export async function meetingPreflight(
+  profile: string,
+  rootDir: string | undefined,
+  probeUserId?: string,
+): Promise<MeetingPreflight> {
+  return checkMeetingPreflight({
+    profile,
+    ...(rootDir ? { rootDir } : {}),
+    ...(probeUserId ? { probeUserId } : {}),
+  });
+}
+
+/** Join a meeting by 9-digit number from the console. */
+export async function meetingJoin(
+  manager: MeetingManager | undefined,
+  body: unknown,
+): Promise<{ ok: true; session: MeetingSessionStatus }> {
+  if (!manager) throw new ApiError(400, '会议能力不可用：profile 未在线或未启用');
+  const fv = asRecord(body);
+  const meetingNo = typeof fv.meetingNo === 'string' ? fv.meetingNo.replace(/\s/g, '') : '';
+  if (!isMeetingNo(meetingNo)) throw new ApiError(400, '会议号必须是 9 位数字');
+  try {
+    const session = await manager.join(meetingNo);
+    return { ok: true, session: session.status() };
+  } catch (err) {
+    throw new ApiError(400, describeMeetingError(err));
+  }
+}
+
+/** Leave a meeting from the console. */
+export async function meetingLeave(
+  manager: MeetingManager | undefined,
+  body: unknown,
+): Promise<{ ok: boolean }> {
+  if (!manager) throw new ApiError(400, '会议能力不可用：profile 未在线或未启用');
+  const fv = asRecord(body);
+  const meetingId = typeof fv.meetingId === 'string' ? fv.meetingId : '';
+  if (!meetingId) throw new ApiError(400, 'meetingId is required');
+  return { ok: await manager.leave(meetingId) };
 }
 
 // ── user-identity group picker (lark-cli, owner's authorization) ─────────────

KeepSilenceQP/lark-coding-agent-bridge

src/ui/api.ts
@@ -11,6 +11,10 @@ import {
   startDeviceLogin,
   type AddBotResult,
 } from '../lark-cli/user-im';
+import { isMeetingNo } from '../meeting/api';
+import { checkMeetingPreflight, type MeetingPreflight } from '../meeting/preflight';
+import { describeMeetingError, type MeetingManager, type MeetingPushHealth } from '../meeting/manager';
+import type { MeetingSessionStatus } from '../meeting/session';
 import { resolveAppPaths } from '../config/app-paths';
 import { loadRootConfig, runtimeProfileConfig } from '../config/profile-store';
 import {
@@ -33,6 +37,7 @@ import {
 import {
   effectiveLarkCliIdentity,
   type GroupResponseMode,
+  type MeetingConfig,
   type LarkCliIdentityPreset,
   type ProfileAccess,
   type ProfileMode,
@@ -60,6 +65,7 @@ export interface ConfigView {
   requireMentionInGroup: boolean;
   groupResponseMode: GroupResponseMode;
   larkCliIdentity: LarkCliIdentityPreset;
+  meeting: MeetingConfig;
   access: {
     allowedUsers: string[];
     allowedChats: string[];
@@ -89,6 +95,7 @@ export function buildConfigView(state: MutableProfileState, live = false): Confi
     requireMentionInGroup: getRequireMentionInGroup(state.cfg),
     groupResponseMode: state.profileConfig.access.groupResponseMode,
     larkCliIdentity: state.profileConfig.larkCli.identityPreset,
+    meeting: state.profileConfig.meeting,
     access: {
       allowedUsers: state.profileConfig.access.allowedUsers,
       allowedChats: state.profileConfig.access.allowedChats,
@@ -144,8 +151,46 @@ function clampInt(value: unknown, min: number, max: number, fallback: number): n
   return Math.min(max, Math.max(min, Math.floor(n)));
 }
 
+/**
+ * Parse the in-meeting agent section. Unspecified fields keep their current
+ * value; numbers are clamped to the same bounds the schema normalizer uses.
+ */
+function parseMeetingBody(body: unknown, current: MeetingConfig): MeetingConfig {
+  if (!body || typeof body !== 'object' || Array.isArray(body)) return current;
+  const fv = body as Record<string, unknown>;
+  const t = (fv.transcript && typeof fv.transcript === 'object' ? fv.transcript : {}) as Record<string, unknown>;
+  const trigger = typeof fv.trigger === 'string' && fv.trigger.trim() ? fv.trigger.trim() : current.trigger;
+  return {
+    enabled: typeof fv.enabled === 'boolean' ? fv.enabled : current.enabled,
+    autoJoinOnInvite:
+      typeof fv.autoJoinOnInvite === 'boolean' ? fv.autoJoinOnInvite : current.autoJoinOnInvite,
+    transcript: {
+      keep: t.keep === undefined ? current.transcript.keep : clampInt(t.keep, 10, 2000, current.transcript.keep),
+      stabilizeMs:
+        t.stabilizeMs === undefined
+          ? current.transcript.stabilizeMs
+          : clampInt(t.stabilizeMs, 0, 30_000, current.transcript.stabilizeMs),
+    },
+    respondIn:
+      fv.respondIn === 'meeting' || fv.respondIn === 'im' || fv.respondIn === 'both'
+        ? fv.respondIn
+        : current.respondIn,
+    trigger,
+    pollIntervalMs:
+      fv.pollIntervalMs === undefined
+        ? current.pollIntervalMs
+        : clampInt(fv.pollIntervalMs, 1000, 60_000, current.pollIntervalMs),
+    summaryOnEnd: typeof fv.summaryOnEnd === 'boolean' ? fv.summaryOnEnd : current.summaryOnEnd,
+    summaryTarget:
+      fv.summaryTarget === 'origin' || fv.summaryTarget === 'owner'
+        ? fv.summaryTarget
+        : current.summaryTarget,
+  };
+}
+
 interface ParsedConfig {
   mode: ProfileMode;
+  meeting: MeetingConfig;
   larkCliIdentity: LarkCliIdentityPreset;
   requireMentionInGroup: boolean;
   groupResponseMode: GroupResponseMode;
@@ -213,11 +258,14 @@ function parseConfigBody(state: MutableProfileState, body: unknown): ParsedConfi
       ? (fv.requireMentionInGroup ? 'mention-only' : 'all-messages')
       : state.profileConfig.access.groupResponseMode;
 
+  const meeting = parseMeetingBody(fv.meeting, state.profileConfig.meeting);
+
   const nextEffectiveIdentity: LarkCliIdentityPreset = mode === 'team' ? 'bot-only' : larkCliIdentity;
   const previousEffectiveIdentity = effectiveLarkCliIdentity(state.profileConfig);
 
   return {
     mode,
+    meeting,
     larkCliIdentity,
     requireMentionInGroup,
     groupResponseMode,
@@ -251,7 +299,14 @@ export async function applyConfig(rt: UiRuntime, body: unknown): Promise<ConfigV
       if (!ok) throw new ApiError(500, 'lark-cli 身份策略未生效');
       identityApplied = true;
     }
-    await savePreferencesConfig(rt, p.nextPreferences, p.groupResponseMode, p.larkCliIdentity, p.mode);
+    await savePreferencesConfig(
+      rt,
+      p.nextPreferences,
+      p.groupResponseMode,
+      p.larkCliIdentity,
+      p.mode,
+      p.meeting,
+    );
   } catch (err) {
     if (identityApplied) {
       await applyProfileLarkCliIdentity(rt, p.previousEffectiveIdentity).catch(() =>
@@ -278,7 +333,14 @@ export async function applyConfigToDisk(
 ): Promise<ConfigView> {
   const p = parseConfigBody(state, body);
   try {
-    await savePreferencesConfig(state, p.nextPreferences, p.groupResponseMode, p.larkCliIdentity, p.mode);
+    await savePreferencesConfig(
+      state,
+      p.nextPreferences,
+      p.groupResponseMode,
+      p.larkCliIdentity,
+      p.mode,
+      p.meeting,
+    );
   } catch (err) {
     throw new ApiError(500, `保存失败：${err instanceof Error ? err.message : String(err)}`);
   }
@@ -367,6 +429,86 @@ export async function listChats(channel: LarkChannel | undefined) {
   if (!channel) return { chats: [] };
   const chats = await fetchKnownChats(channel).catch(() => []);
   return { chats };
+}
+
+// ── in-meeting agent ─────────────────────────────────────────────────────────
+
+export interface MeetingsView {
+  /** False when the profile is offline or `meeting.enabled` is off. */
+  available: boolean;
+  reason?: string;
+  sessions: MeetingSessionStatus[];
+  push: MeetingPushHealth;
+}
+
+/**
+ * Live meeting state for a profile. Needs the running process's manager, so an
+ * offline profile reports `available: false` rather than erroring.
+ */
+export function meetingsView(manager: MeetingManager | undefined, enabled: boolean): MeetingsView {
+  if (!enabled) {
+    return {
+      available: false,
+      reason: '该 profile 未启用会议智能体',
+      sessions: [],
+      push: { hooked: false, received: 0 },
+    };
+  }
+  if (!manager) {
+    return {
+      available: false,
+      reason: 'profile 未在线（会议能力只在运行中的进程里可用）',
+      sessions: [],
+      push: { hooked: false, received: 0 },
+    };
+  }
+  return { available: true, sessions: manager.list(), push: manager.pushHealth() };
+}
+
+/**
+ * Whether the app identity can actually join meetings. Runs a read-only probe;
+ * when a scope is missing the result carries Feishu's own scope-apply URL so
+ * the console can offer a one-click grant.
+ */
+export async function meetingPreflight(
+  profile: string,
+  rootDir: string | undefined,
+  probeUserId?: string,
+): Promise<MeetingPreflight> {
+  return checkMeetingPreflight({
+    profile,
+    ...(rootDir ? { rootDir } : {}),
+    ...(probeUserId ? { probeUserId } : {}),
+  });
+}
+
+/** Join a meeting by 9-digit number from the console. */
+export async function meetingJoin(
+  manager: MeetingManager | undefined,
+  body: unknown,
+): Promise<{ ok: true; session: MeetingSessionStatus }> {
+  if (!manager) throw new ApiError(400, '会议能力不可用：profile 未在线或未启用');
+  const fv = asRecord(body);
+  const meetingNo = typeof fv.meetingNo === 'string' ? fv.meetingNo.replace(/\s/g, '') : '';
+  if (!isMeetingNo(meetingNo)) throw new ApiError(400, '会议号必须是 9 位数字');
+  try {
+    const session = await manager.join(meetingNo);
+    return { ok: true, session: session.status() };
+  } catch (err) {
+    throw new ApiError(400, describeMeetingError(err));
+  }
+}
+
+/** Leave a meeting from the console. */
+export async function meetingLeave(
+  manager: MeetingManager | undefined,
+  body: unknown,
+): Promise<{ ok: boolean }> {
+  if (!manager) throw new ApiError(400, '会议能力不可用：profile 未在线或未启用');
+  const fv = asRecord(body);
+  const meetingId = typeof fv.meetingId === 'string' ? fv.meetingId : '';
+  if (!meetingId) throw new ApiError(400, 'meetingId is required');
+  return { ok: await manager.leave(meetingId) };
 }
 
 // ── user-identity group picker (lark-cli, owner's authorization) ─────────────